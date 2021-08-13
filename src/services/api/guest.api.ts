681514d53bc5fb81ffd8e74c4bfd44b314da3319
csulit <chrisgelosulit@gmail.com>
2021-08-13 15:09:29 +0800

    Removed visitId return on guest booking

--- src/services/api/guest.api.ts
+++ src/services/api/guest.api.ts
@@ -2,9 +2,7 @@ import { makeRequest } from '../http.service';
 
 export class GuestAPI {
   static async createVisit(data: any) {
-    const createVisit = await makeRequest<{
-      visitorId: string;
-    }>({
+    const createVisit = await makeRequest({
       url: '/api/visitors/guests',
       method: 'POST',
       data,
