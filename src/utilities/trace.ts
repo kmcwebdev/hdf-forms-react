79485f248122a2ad5d8aef5bf26b4540387540a4
Christian Angelo M Sulit <chrisgelosulit@gmail.com>
2021-09-08 17:30:53 +0800

    Added trace.ts

---
+++ src/utilities/trace.ts
@@ -0,0 +1,23 @@
+export const cityTrackingURLs = [
+  {
+    name: 'Pasig Pass',
+    content:
+      'In compliance with Pasig City Ordinance No. 49 s.2020, all residents and non-residents of the city, establishments within the city, whether public or private are mandated to register to <strong>Pasig Pass - Digital Contract Tracing</strong>.',
+    city: 'Pasig',
+    registrationLink: 'https://pasigpass.pasigcity.gov.ph/',
+  },
+  {
+    name: 'MandaTrack',
+    content:
+      'In compliance with MandaTrack Ordinance, all residents and non-residents of the city, establishments within the city, whether public or private are mandated to register to <strong>MandaTrack - Digital Contract Tracing</strong>.',
+    city: 'Mandaluyong',
+    registrationLink: 'https://mandatrack.appcase.net/',
+  },
+  {
+    name: 'Taguig T.R.A.C.E',
+    content:
+      'For	purposes of going out	of their homes for acquiring essential goods or services or	other	allowed	essential	purposes,	Taguig residents may use their Taguig	Unified	Quarantine Pass	(UQP) or any	valid	government issued ID indicating	their	place	of	residence.',
+    city: 'Taguig',
+    registrationLink: 'https://trace.taguig.gov.ph/',
+  },
+];
